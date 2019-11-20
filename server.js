79ba0ecb2230dac3254a912c43bd77e9d3933e10
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-20 06:14:59 +0500

    run npm install first .then resttart server.

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = 3000;
+var bodyParser = require('body-parser')
 
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('server works!'));
 
 notes = [];
 
-app.post('/add-note',(req, res)=>{
-    console.log(req.query)
-    note = {title: req.query.title, description: req.query.description};
+app.get('/get-notes',(req,res)=>{
+    console.log('get notes api hit')
+    return res.send(notes);
+});
 
+app.post('/add-note',(req, res)=>{
+    console.log(req.body)
+    note = {title: req.body.title, description: req.body.description};
     notes.push(note)
-    res.send(notes)
+    res.json(notes)
 })
 
 
 app.post('/remove-note',(req, res)=>{
-    console.log(req.query)
-    note = {title: req.query.title, description: req.query.description};
+    console.log(req.body)
+    note = {title: req.body.title, description: req.body.description};
     var index = notes.indexOf(note); // get index if value found otherwise -1
     for(let i=0; i<notes.length;i++){
         if(notes[i].title === note.title && notes[i].description === note.description)index = i;
